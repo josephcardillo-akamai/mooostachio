4d43132f0cf259d177d61a79ad189bca4b679061
joseph.cardillo <joseph.s.cardillo@gmail.com>
2018-05-10 16:27:35 -0400

    Added redirects when visiting localhost based on logged_in status. Closes #6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [:email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+
+  private
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(_resource_or_scope)
+    new_user_session_path
+  end
+
+  def after_sign_in_path_for(_resource_or_scope)
+    root_path(current_user)
+  end
 end
