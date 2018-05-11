c2e0d30f38a0cd1e1b188b338a75d8d60de6419a
joseph.cardillo <joseph.s.cardillo@gmail.com>
2018-05-11 17:44:46 -0400

    Showed checking balance on dashboard. Closes #50.

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,12 @@
 module DashboardHelper
-end
+  def account_sum
+    @mentries = Mentry.all
+    total = 0
+      @mentries.each do |mentry|
+        if mentry.maccount.name == "Checking"
+        total += mentry.amount
+        end
+      end
+      number_to_currency(total)
+    end
+  end
