1bd663e68d02d889f03acdd86d11b8ee8e8d8047
joseph.cardillo <joseph.s.cardillo@gmail.com>
2018-05-11 10:15:55 -0400

    Added downcase! before_save validation to accounts, cats, status, type and location. Closes #16.

--- app/models/maccount.rb
+++ app/models/maccount.rb
@@ -1,8 +1,11 @@
 class Maccount < ApplicationRecord
   has_many :mentries
   belongs_to :user
-
-  validates :name, uniqueness: true
+  before_save :downcase_fields
+  before_save validates :name, uniqueness: true
   validates_length_of :name, :maximum => 50, :allow_blank => false
 
+  def downcase_fields
+    self.name.downcase!
+  end
 end
--- app/models/mlocation.rb
+++ app/models/mlocation.rb
@@ -2,6 +2,11 @@ class Mlocation < ApplicationRecord
   has_many :mentries
   belongs_to :user
 
-  validates :name, uniqueness: true
+  before_save :downcase_fields
+  before_save validates :name, uniqueness: true
   validates_length_of :name, :maximum => 50, :allow_blank => false
+
+  def downcase_fields
+    self.name.downcase!
+  end
 end
