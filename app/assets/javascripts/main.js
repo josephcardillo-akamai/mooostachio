607d04873fc0ec3cd54fd9289530c6f3fc257e94
joseph.cardillo <joseph.s.cardillo@gmail.com>
2018-05-14 13:12:03 -0400

    Transfer functionality works. Closes #29

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -16,3 +16,23 @@ var select_amount;
 $(document).on('input','#mentry_amount', function(){
   select_amount = $('#mentry_amount').val();
 });
+
+var select_status;
+$(document).on('change','#mentry_mstatus_id', function(){
+  select_status = $('#mentry_mstatus_id').val();
+});
+
+var select_account;
+$(document).on('change','#mentry_maccount_id', function(){
+  select_account = $('#mentry_maccount_id').val();
+});
+
+var select_category;
+$(document).on('change','#mentry_mcategory_id', function(){
+  select_category = $('#mentry_mcategory_id').val();
+});
+
+var select_location;
+$(document).on('change','#mentry_mlocation_id', function(){
+  select_location = $('#mentry_mlocation_id').val();
+});
