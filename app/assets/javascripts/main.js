95935d385b3208d58f7ec56eaebb75793528b175
joseph.cardillo <joseph.s.cardillo@gmail.com>
2018-05-14 11:02:21 -0400

    UJS to update new entry form when transfer is selected. In progress.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,1 +1,12 @@
 
+var select_transfer;
+$(document).on('change','#transfer', function(){
+  select_transfer = $('#transfer').val();
+  if (select_transfer == 3) {
+    // alert("You selected transfer!");
+    $('#transfer-btn').replaceWith("<input type='submit' name='commit' value='Create Entry' id='transfer-btn' data-disable-with='Create Entry'>");
+  }
+  else {
+    alert("What's going on here?");
+  }
+});
