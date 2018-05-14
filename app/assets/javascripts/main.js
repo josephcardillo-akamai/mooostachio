5990bde99b35b6375da2f702848fb1ed30ec6cc4
joseph.cardillo <joseph.s.cardillo@gmail.com>
2018-05-14 12:36:47 -0400

    When transferring, form submits then replaces with new form with amount * -1 to transfer to new account.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,9 +4,15 @@ $(document).on('change','#transfer', function(){
   select_transfer = $('#transfer').val();
   if (select_transfer == 3) {
     // alert("You selected transfer!");
-    $('#transfer-btn').replaceWith("<input type='submit' name='commit' value='Create Entry' id='transfer-btn' data-disable-with='Create Entry'>");
+    $('#transfer-btn').replaceWith("<input type='submit' name='commit' value='Transfer To...' id='transfer-btn' data-disable-with='Create Entry'>");
   }
   else {
     alert("What's going on here?");
   }
+});
+
+
+var select_amount;
+$(document).on('input','#mentry_amount', function(){
+  select_amount = $('#mentry_amount').val();
 });
