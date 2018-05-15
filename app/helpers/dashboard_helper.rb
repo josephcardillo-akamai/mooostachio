22f907f432f72eeabf0ccc2f3d4ce12b6784742a
joseph.cardillo <joseph.s.cardillo@gmail.com>
2018-05-15 13:54:43 -0400

    Created chart array helper to push first category and amount into array and display chart on dashboard.

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -10,4 +10,20 @@ module DashboardHelper
       number_to_currency(total)
   end
 
+  def chart_array
+    @categories = Mcategory.all
+    @entries = Mentry.all
+    cat_amount_array = []
+    chart_array = []
+    @categories.each do |category|
+      @entries.each do |entry|
+        if category.name == entry.mcategory.name
+        cat_amount_array.push(category.name)
+        cat_amount_array.push(entry.amount)
+        end
+        chart_array.push(cat_amount_array)
+        return chart_array
+      end
+    end
+  end
 end
