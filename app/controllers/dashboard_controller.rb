1f78e281bc78daed11f853ee68c5f64ac42be888
joseph.cardillo <joseph.s.cardillo@gmail.com>
2018-05-21 17:27:47 -0400

    Dashboard index view.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,59 +7,59 @@ class DashboardController < ApplicationController
     @mentry = Mentry.new
     @entry = Mentry.first
 
-    if params[:to_date] != nil && params[:from_date] != nil
-      # Set "to_date" params
-      @to_date_year = "#{params[:to_date]['(1i)']}"
-      @to_date_month = "#{params[:to_date]['(2i)']}"
-      @to_date_day = "#{params[:to_date]['(3i)']}"
+    # if params[:to_date] != nil && params[:from_date] != nil
+    #   # Set "to_date" params
+    #   @to_date_year = "#{params[:to_date]['(1i)']}"
+    #   @to_date_month = "#{params[:to_date]['(2i)']}"
+    #   @to_date_day = "#{params[:to_date]['(3i)']}"
+    #
+    #   # Set "from_date" params
+    #   @from_date_year = "#{params[:from_date]['(1i)']}"
+    #   @from_date_month = "#{params[:from_date]['(2i)']}"
+    #   @from_date_day = "#{params[:from_date]['(3i)']}"
+    #
+    #   # To convert "to_date" month to two digits if needed
+    #   if @to_date_month.to_i < 10
+    #     @to_date_month_two_digit = "0" + "#{@to_date_month}"
+    #   else
+    #     @to_date_month_two_digit = @to_date_month
+    #   end
+    #
+    #   # To convert "to_date" day to two digits if needed
+    #   if @to_date_day.to_i < 10
+    #     @to_date_day_two_digit = "0" + "#{@to_date_day}"
+    #   else
+    #     @to_date_day_two_digit = @to_date_day
+    #   end
+    #
+    #
+    #   # To convert "to_date" month to two digits if needed
+    #   if @from_date_month.to_i < 10
+    #     @from_date_month_two_digit = "0" + "#{@from_date_month}"
+    #   else
+    #     @from_date_month_two_digit = @from_date_month
+    #   end
+    #
+    #   # To convert "from_date" day from two digits if needed
+    #   if @from_date_day.to_i < 10
+    #     @from_date_day_two_digit = "0" + "#{@from_date_day}"
+    #   else
+    #     @from_date_day_two_digit = @from_date_day
+    #   end
+    #
+    #
+    #   # To convert "from_date" into correct format
+    #   @from_date = "#{@from_date_year}-#{@from_date_month_two_digit}-#{@from_date_day}"
+    #
+    #   # To convert "to_date" into correct format
+    #   @to_date = "#{@to_date_year}-#{@to_date_month_two_digit}-#{@to_date_day}"
 
-      # Set "from_date" params
-      @from_date_year = "#{params[:from_date]['(1i)']}"
-      @from_date_month = "#{params[:from_date]['(2i)']}"
-      @from_date_day = "#{params[:from_date]['(3i)']}"
-
-      # To convert "to_date" month to two digits if needed
-      if @to_date_month.to_i < 10
-        @to_date_month_two_digit = "0" + "#{@to_date_month}"
-      else
-        @to_date_month_two_digit = @to_date_month
-      end
-
-      # To convert "to_date" day to two digits if needed
-      if @to_date_day.to_i < 10
-        @to_date_day_two_digit = "0" + "#{@to_date_day}"
-      else
-        @to_date_day_two_digit = @to_date_day
-      end
-
-
-      # To convert "to_date" month to two digits if needed
-      if @from_date_month.to_i < 10
-        @from_date_month_two_digit = "0" + "#{@from_date_month}"
-      else
-        @from_date_month_two_digit = @from_date_month
-      end
-
-      # To convert "from_date" day from two digits if needed
-      if @from_date_day.to_i < 10
-        @from_date_day_two_digit = "0" + "#{@from_date_day}"
-      else
-        @from_date_day_two_digit = @from_date_day
-      end
-
-
-      # To convert "from_date" into correct format
-      @from_date = "#{@from_date_year}-#{@from_date_month_two_digit}-#{@from_date_day}"
-
-      # To convert "to_date" into correct format
-      @to_date = "#{@to_date_year}-#{@to_date_month_two_digit}-#{@to_date_day}"
-
-    end
-
-    puts '****************************'
-    puts @from_date
-    puts @to_date
-    puts '****************************'
+    # end
+    #
+    # puts '****************************'
+    # puts @from_date
+    # puts @to_date
+    # puts '****************************'
   end
 
   def show
@@ -85,9 +85,9 @@ class DashboardController < ApplicationController
     end
 
     if @mentry.save
-      puts '****************************'
-      puts params.inspect
-      puts '****************************'
+      # puts '****************************'
+      # puts params.inspect
+      # puts '****************************'
       respond_to do |format|
         format.js
       end
